94729ae57202b67e785c9774c5108f9c7009a06e
acn-r2d2 <r2d2@accenture.com>
2026-02-24 11:27:36 +0100

    feat: install ripgrep globally

--- bricks/install-base-tools.sh
+++ bricks/install-base-tools.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 CONFTEST_VERSION=0.66.0
+RIPGREP_VERSION=15.1.0
 
 apt-get update
 apt-get install -y unzip curl git
@@ -10,3 +11,6 @@ curl -fsSL --retry 5 --retry-delay 5 "https://github.com/open-policy-agent/conft
 tar -xzf /usr/local/bin/conftest.tar.gz -C /usr/local/bin conftest
 chmod +x /usr/local/bin/conftest
 rm -f /usr/local/bin/conftest.tar.gz
+
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep_${RIPGREP_VERSION}-1_amd64.deb
+dpkg -i ripgrep_${RIPGREP_VERSION}-1_amd64.deb
