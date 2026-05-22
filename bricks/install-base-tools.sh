a24b484233ccc064b25211ebfe57b58f1b47f0d3
acn-r2d2 <r2d2@accenture.com>
2026-05-22 09:57:15 +0200

    feat: install git-cliff

--- bricks/install-base-tools.sh
+++ bricks/install-base-tools.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 CONFTEST_VERSION=0.68.2
 RIPGREP_VERSION=15.1.0
+GIT_CLIFF_VERSION=2.13.1
 
 apt-get update
 apt-get install -y unzip curl git
@@ -14,3 +15,8 @@ rm -f /usr/local/bin/conftest.tar.gz
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep_${RIPGREP_VERSION}-1_amd64.deb
 dpkg -i ripgrep_${RIPGREP_VERSION}-1_amd64.deb
+
+curl -fsSL --retry 5 --retry-delay 5 "https://github.com/orhun/git-cliff/releases/download/v${GIT_CLIFF_VERSION}/git-cliff-${GIT_CLIFF_VERSION}-x86_64-unknown-linux-gnu.tar.gz" -o /usr/local/bin/git-cliff.tar.gz
+tar -xvzf /usr/local/bin/git-cliff.tar.gz -C /usr/local/bin
+ln -s /usr/local/bin/git-cliff-${GIT_CLIFF_VERSION}/git-cliff /usr/local/bin/git-cliff
+rm -f /usr/local/bin/git-cliff.tar.gz
